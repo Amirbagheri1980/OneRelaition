724d914ec18ce7c1fcf6474af26d39061d276273
Amir Bagheri <Amirbagheri1980@gamil.com>
2018-04-10 15:35:19 +0930

    Class Cars

---
+++ RelationShips/RelationShips/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RelationShips.MvcApplication" Language="C#" %>
---
+++ RelationShips/RelationShips/Models/Cars.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace RelationShips.Models
+{
+    public class Cars
+    {
+       [Key]
+       [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Display(Name ="Make")]
+        public string Make { get; set; }
+        [Display(Name ="Model")]
+        public string Model { get; set; }
+    }
+}
